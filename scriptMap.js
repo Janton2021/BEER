be1fff9eb168c8834c0ea9cc19c121338694f4a6
Juan Anton <jantonperez1980@gmail.com>
2021-06-14 17:47:59 +0200

    Modificacion de scriptMap.js

--- scriptMap.js
+++ scriptMap.js
@@ -1,4 +1,4 @@
-let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+let mymap = L.map('mapid').setView([40.4167, -3.70325], 13);
 
 let MAPBOX_TOKEN = 'pk.eyJ1IjoiamFudG9uMTk4MCIsImEiOiJja3BzaXd4Mm8wNHF4MnZwaWNydGNjZHdhIn0.duWzFaJJS05KjayajI9bWQ'
 
@@ -11,31 +11,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: MAPBOX_TOKEN
 }).addTo(mymap);
 
-function geoFindMe() {
-    var output = document.getElementById("out");
-  
-    if (!navigator.geolocation){
-      output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-      return;
-    }
-  
-    function success(position) {
-      var latitude  = position.coords.latitude;
-      var longitude = position.coords.longitude;
-  
-      output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-  
-      var img = new Image();
-      img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-  
-      output.appendChild(img);
-    };
-  
-    function error() {
-      output.innerHTML = "Unable to retrieve your location";
-    };
-  
-    output.innerHTML = "<p>Locating…</p>";
-  
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
+let marker = L.marker([40.4167, -3.70325]).addTo(mymap);
+
+marker.bindPopup("<b>Aquí estoy!</b><br>Perdido con una cerveza.").openPopup();
+
